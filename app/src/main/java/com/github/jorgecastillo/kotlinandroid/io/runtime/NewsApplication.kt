d3a69d91b8c64bf9aecd4e8797603c26b32d8d2d
Stefano Pian <stefano.pian.1985@gmail.com>
2020-08-16 01:22:34 +0200

    updated deps

--- app/src/main/java/com/github/jorgecastillo/kotlinandroid/io/runtime/NewsApplication.kt
+++ app/src/main/java/com/github/jorgecastillo/kotlinandroid/io/runtime/NewsApplication.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import com.github.jorgecastillo.kotlinandroid.io.algebras.data.network.NewsApiService
 import com.github.jorgecastillo.kotlinandroid.io.algebras.data.network.NewsAuthInterceptor
 import com.github.jorgecastillo.kotlinandroid.io.runtime.context.RuntimeContext
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Dispatchers
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -21,9 +23,15 @@ class NewsApplication : Application() {
     }
 
     private val newsService: NewsApiService by lazy {
+        val moshi = Moshi.Builder()
+                         .add(KotlinJsonAdapterFactory())
+                         .build()
+
+        val converterFactory = MoshiConverterFactory.create(moshi)
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://newsapi.org/v2/")
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(converterFactory)
             .client(httpClient)
             .build()
 
